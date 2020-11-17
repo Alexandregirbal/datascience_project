8a0f6af18afad1aa4155ec1a9dc5355bc44e790e
David Meier <davidmeier@hotmail.fr>
2020-11-17 21:31:43 +0100

    generateTopNSenders

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 
 # Functions
 from getEmails import getEmails
-from generateWeeklyTrafic import generateWeeklyTraficGraph
-
+#from generateWeeklyTrafic import generateWeeklyTraficGraph
+from generateTopNSenders import generateTopNSenders
 # config variables
 from config import username
 from config import password
@@ -16,13 +16,17 @@ if sender == 0:
     sender = input("Enter the email adresse of the sender: ")
 
 if beginDate == 0:
-    beginDate = input("Enter the begin date of the emails to treat (ex: 01-Aug-2019): ")
+    beginDate = input(
+        "Enter the begin date of the emails to treat (ex: 01-Aug-2019): ")
 
 if endDate == 0:
-    endDate = input("Enter the end date of the emails to treat(ex: 31-Aug-2019):")
+    endDate = input(
+        "Enter the end date of the emails to treat(ex: 31-Aug-2019):")
 
 if analysis == 0:
     analysis = input("Enter the analysis type(\n1:Remi\n2:David\n3:Alex\n--->")
 
-pandasDataFrameOfEmails = getEmails(username, password, 5, sender, beginDate, endDate)
-generateWeeklyTraficGraph(pandasDataFrameOfEmails)
+pandasDataFrameOfEmails = getEmails(
+    username, password, 5, sender, beginDate, endDate)
+# generateWeeklyTraficGraph(pandasDataFrameOfEmails)
+generateTopNSenders(pandasDataFrameOfEmails)
